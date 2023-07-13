4386cbcea5f4d2666722794d5dc7378dec2c7c85
Dr.Xiong <1393744283@qq.com>
2023-07-13 23:57:34 +0800

    feat navigation

--- src/components/question/MathQuestion.vue
+++ src/components/question/MathQuestion.vue
@@ -6,9 +6,11 @@
       </div>
       <q-card-section>
         <div class="q-px-md" style="display: block;">
-          {{ questionId }}.
-          <KatexFormula v-if="!imgMod" :formula="question" />
-          <img v-else :src="question" />
+          <span class="row items-center">
+            <span class="q-pr-xs"> {{ questionId + "." }}</span>
+            <KatexFormula v-if="!imgMod" :formula="question" />
+            <img v-else :src="question" />
+          </span>
         </div>
       </q-card-section>
       <q-separator inset />
@@ -16,18 +18,24 @@
       <q-card-section class="q-pb-xs">
         <q-option-group v-model="group" :options="options" color="primary">
           <template v-slot:label="opt">
-            <KatexFormula v-if="!imgMod" :class="{
-              'text-green': check && opt.value == answerIndex,
-              'text-red': check && opt.value != answerIndex && opt.value == group
-            }" :formula="opt.label" />
-            <img v-else :src="opt.label" />
+            <span class="row items-center">
+              <span :class="['q-pr-xs', {
+                'text-green': check && opt.value == answerIndex,
+                'text-red': check && opt.value != answerIndex && opt.value == group
+              }]">{{ String.fromCharCode("A".charCodeAt(0) + opt['index']) + "." }}</span>
+              <KatexFormula v-if="!imgMod" :class="{
+                'text-green': check && opt.value == answerIndex,
+                'text-red': check && opt.value != answerIndex && opt.value == group
+              }" :formula="opt.label" />
+              <img v-else :src="opt.label" />
+            </span>
           </template>
         </q-option-group>
       </q-card-section>
 
       <q-card-section class="q-py-xs">
         <q-slide-transition>
-          <div v-show="visible">
+          <div v-show="check">
             解析：
             <KatexFormula v-if="!imgMod" :formula="hint" />
             <img v-else :src="hint" />
@@ -37,12 +45,11 @@
 
       <q-card-section class="row justify-center q-pt-sm">
         <q-btn-group push class="full-width">
-          <q-btn push class="full-width" label="检查" icon="done" 
-          :color="check ? 'primary' : 'white'" :text-color="check ? 'white' : 'black'"
-            @click="() => { check = !check }" />
-          <q-btn push class="full-width" label="解析" icon="manage_search" 
-          :color="visible ? 'primary' : 'white'" :text-color="visible ? 'white' : 'black'"
-            @click="() => { visible = !visible }" />
+          <q-btn push class="full-width" label="上一题" icon="keyboard_arrow_left" @click="goto(-1)" />
+          <q-btn push style="border-left: 1px solid rgba(0, 0, 0, 0.15);border-right: 1px solid rgba(0, 0, 0, 0.15)"
+            class="full-width" label="答案解析" icon="done" :color="check ? 'primary' : 'white'"
+            :text-color="check ? 'white' : 'black'" @click="() => { check = !check }" />
+          <q-btn push class="full-width" label="下一题" icon="keyboard_arrow_right" @click="goto(1)" />
         </q-btn-group>
       </q-card-section>
     </q-card>
@@ -53,6 +60,8 @@
 import { computed, ref } from 'vue';
 import KatexFormula from '../katex/KatexFormula.vue';
 import { shuffle } from '../../utils/array'
+import { useRouter } from 'vue-router';
+const router = useRouter();
 
 const props = defineProps<{
   question: string
@@ -75,18 +84,24 @@ for (let i = 0; i < props.choices.length; i++) {
 }
 shuffle(map);
 const options = computed(() => {
-  const opts = props.choices.map((choice, index) => {
-    return {
-      value: index,
-      label: choice,
-      color: !check.value ? "primary" : (index == props.answerIndex ? "green" : "red")
-    }
-  })
-  return opts.map((opt, index) => opts[map[index]]);
+  const opts = props.choices
+    .map((choice, index) => {
+      return {
+        value: index,
+        index: 0,
+        label: choice,
+        color: !check.value ? "primary" : (index == props.answerIndex ? "green" : "red")
+      }
+    })
+  return opts
+    .map((opt, index) => opts[map[index]])
+    .map((opt, index) => {
+      opt.index = index;
+      return opt
+    });
 });
 
 const group = ref();
-const visible = ref(false);
 const check = ref(false);
 
 const diffTag = computed(() => {
@@ -103,6 +118,14 @@ const diffTag = computed(() => {
   }
   return ans;
 })
+
+const goto = (offset: number) => {
+
+  const currentPath = router.currentRoute.value.path
+  const parentPath = currentPath.substring(0, currentPath.lastIndexOf('/'))
+  const lastPath = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+  router.push(parentPath + '/' + (parseInt(lastPath) + offset));
+}
 
 </script>
 
