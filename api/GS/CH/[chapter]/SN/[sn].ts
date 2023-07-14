51fc592fcbad71f37b69efb3f1913237992dbb3d
Dr.Xiong <1393744283@qq.com>
2023-07-14 12:24:16 +0800

    feat serverless FC

---
+++ api/GS/CH/[chapter]/SN/[sn].ts
@@ -0,0 +1,86 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import TableStore from 'tablestore';
+const Long = TableStore.Long;
+
+const client = new TableStore.Client({
+  accessKeyId: process.env.AccessKey_ID,
+  accessKeySecret: process.env.AccessKey_Secret,
+  endpoint: process.env.Endpoint,
+  instancename: process.env.Instancename,
+  maxRetries: 5, //默认20次重试，可以省略此参数。
+});
+
+function base64IfNull(str: string, blob:Buffer) {
+  if (str) {
+    return str;
+  } else {
+    return "data:image/png;base64," + Buffer.from(blob).toString("base64");
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const chapter = parseInt(req.query.chapter as string);
+  const sn = parseInt(req.query.sn as string);
+
+  const params = {
+    tableName: 'IMG_GS',
+    primaryKey: [
+      { CHAPTER: Long.fromNumber(chapter), SN: Long.fromNumber(sn) },
+    ],
+    maxVersions: 1, //最多可读取的版本数，设置为2即代表最多可读取2个版本。
+  };
+
+  await new Promise<TableStore.Row>((resolve, reject) => {
+    client.getRow(params, (err, data) => {
+      if (err || data.row===undefined) {
+        reject(err);
+      } else {
+        resolve(data.row);
+      }
+    });
+  })
+    .then((data) => {
+      const convert: { [key: string]: any } = {};
+      if(!data.attributes){
+        error(res,"no data");
+        return
+      }
+
+      data.attributes.forEach((attr)=>{
+        convert[attr.columnName] = attr.columnValue
+      });
+      
+      success(res, data);
+    })
+    .catch((err) => {
+      error(res,err.message);
+    });
+}
+
+function error(res:NextApiResponse, message:string) {
+  res.status(500).send({
+    success: false,
+    message: message,
+    data: {},
+  });
+}
+
+function success(res:NextApiResponse, data:any) {
+  res.status(200).send({
+    success: true,
+    data: {
+      question: base64IfNull(data.QUESTION, data.IMG0),
+      choices: [
+        base64IfNull(data.CHOICE1, data.IMG1),
+        base64IfNull(data.CHOICE2, data.IMG2),
+        base64IfNull(data.CHOICE3, data.IMG3),
+        base64IfNull(data.CHOICE4, data.IMG4),
+      ],
+      hint: base64IfNull(data.HINT, data.IMG5),
+      difficulty: data.DIFF,
+    },
+  });
+}
